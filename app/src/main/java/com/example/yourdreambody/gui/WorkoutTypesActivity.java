5b5b26ac729906709754746f391ab9ab71530ba5
Brenden7788 <brendenjose2002@gmail.com>
2021-07-08 20:29:25 +0200

    LabelCounters

--- app/src/main/java/com/example/yourdreambody/gui/WorkoutTypesActivity.java
+++ app/src/main/java/com/example/yourdreambody/gui/WorkoutTypesActivity.java
@@ -1,7 +1,5 @@
 package com.example.yourdreambody.gui;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
@@ -40,6 +38,15 @@ public class WorkoutTypesActivity extends Activity {
     TextView mountainCounter;
     TextView crunchesCounter;
     TextView skippingCounter;
+    Integer pushCounter1 = 0;
+    Integer squatCounter1 = 0;
+    Integer jumpingCounter1 = 0;
+    Integer sittupCounter1 = 0;
+    Integer buerpeeCounter1 = 0;
+    Integer plankCounter1 = 0;
+    Integer mountainCounter1 = 0;
+    Integer crunchesCounter1 = 0;
+    Integer skippingCounter1 = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -49,7 +56,30 @@ public class WorkoutTypesActivity extends Activity {
         submit = findViewById(R.id.submitButton);
         intent = new Intent(this, WorkoutActivity.class);
 
-        pushCounter = findViewById(R.id.pushCounter);
+
+        //Buttons
+        plus1 = findViewById(R.id.plus1);
+        plus2 = findViewById(R.id.plus2);
+        plus3 = findViewById(R.id.plus3);
+        plus4 = findViewById(R.id.plus4);
+        plus5 = findViewById(R.id.plus5);
+        plus6 = findViewById(R.id.plus6);
+        plus7 = findViewById(R.id.plus7);
+        plus8 = findViewById(R.id.plus8);
+        plus9 = findViewById(R.id.plus9);
+
+        minus1 = findViewById(R.id.minus1);
+        minus2 = findViewById(R.id.minus2);
+        minus3 = findViewById(R.id.minus3);
+        minus4 = findViewById(R.id.minus4);
+        minus5 = findViewById(R.id.minus5);
+        minus6 = findViewById(R.id.minus6);
+        minus7 = findViewById(R.id.minus7);
+        minus8 = findViewById(R.id.minus8);
+        minus9 = findViewById(R.id.minus9);
+
+        //Labels
+        pushCounter = findViewById(R.id.pushCount);
         squatCounter = findViewById(R.id.squatCounter);
         jumpingCounter = findViewById(R.id.jumpCounterCounter);
         sittupCounter = findViewById(R.id.situpCounter);
@@ -59,8 +89,121 @@ public class WorkoutTypesActivity extends Activity {
         crunchesCounter = findViewById(R.id.crunchesCounter);
         skippingCounter = findViewById(R.id.skippingCounter);
 
+        //intent
         submit.setOnClickListener(v -> {
             startActivity(intent);
         });
+
+        //pushups
+        plus1.setOnClickListener(v -> {
+            pushCounter1++;
+            refresh();
+        });
+
+        minus1.setOnClickListener(v -> {
+            pushCounter1--;
+            refresh();
+        });
+
+        //squats
+        plus2.setOnClickListener(v -> {
+            squatCounter1++;
+            refresh();
+        });
+
+        minus2.setOnClickListener(v -> {
+            squatCounter1--;
+            refresh();
+        });
+        //hampelmann
+        plus3.setOnClickListener(v -> {
+            jumpingCounter1++;
+            refresh();
+        });
+
+        minus3.setOnClickListener(v -> {
+            jumpingCounter1--;
+            refresh();
+        });
+
+        //situp
+        plus4.setOnClickListener(v -> {
+            sittupCounter1++;
+            refresh();
+        });
+
+        minus4.setOnClickListener(v -> {
+            sittupCounter1--;
+            refresh();
+        });
+
+        //buerpee
+        plus5.setOnClickListener(v -> {
+            buerpeeCounter1++;
+            refresh();
+        });
+
+        minus5.setOnClickListener(v -> {
+            buerpeeCounter1--;
+            refresh();
+        });
+
+        //plank
+        plus6.setOnClickListener(v -> {
+            plankCounter1++;
+            refresh();
+        });
+
+        minus6.setOnClickListener(v -> {
+            plankCounter1--;
+            refresh();
+        });
+
+        //mountainClimber
+        plus7.setOnClickListener(v -> {
+            mountainCounter1++;
+            refresh();
+        });
+
+        minus7.setOnClickListener(v -> {
+            mountainCounter1--;
+            refresh();
+        });
+
+        //crunches
+        plus8.setOnClickListener(v -> {
+            crunchesCounter1++;
+            refresh();
+        });
+
+        minus8.setOnClickListener(v -> {
+            crunchesCounter1--;
+            refresh();
+        });
+
+        //seilspringen
+        plus9.setOnClickListener(v -> {
+            skippingCounter1++;
+            refresh();
+        });
+
+        minus9.setOnClickListener(v -> {
+            skippingCounter1--;
+            refresh();
+        });
+
+
+    }
+    //refresh labels
+    public void refresh(){
+        pushCounter.setText(pushCounter1.toString());
+        squatCounter.setText(squatCounter1.toString());
+        jumpingCounter.setText(jumpingCounter1.toString());
+        sittupCounter.setText(sittupCounter1.toString());
+        buerpeeCounter.setText(buerpeeCounter1.toString());
+        plankCounter.setText(plankCounter1.toString());
+        mountainCounter.setText(mountainCounter1.toString());
+        crunchesCounter.setText(crunchesCounter1.toString());
+        skippingCounter.setText(skippingCounter1.toString());
     }
 }
